aff42e0d9744e6e4ce69f81b0436091d120c16af
Anvesh <anvkup2002@gmail.com>
2022-08-31 18:59:59 +0530

    Added Colorpallete

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -4,7 +4,7 @@ import './Navbar.css'
 function Navbar() {
   return (
     <div className='navbar'>
-        <span className='navbarHeading'>SFX Marketplace</span>
+        <span className='navbarHeading'>SFX<span>Marketplace</span></span>
           <div className="navMenu">
           <li className="navMenuItem">Home</li>
           <li className="navMenuItem">About</li>
