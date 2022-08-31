bb78bcd7007067cda15272b3e98721e1123fb414
Vishal Vishwajeet <vishalvishwajeet422@gmail.com>
2022-08-31 18:38:39 +0530

    navbar update 2

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './Navbar.css'
 
 function Navbar() {
   return (
     <div className='navbar'>
-        <h1>Navbar</h1>
+        <span className='navbarHeading'>SFX Marketplace</span>
+          <div className="navMenu">
+          <li className="navMenuItem">Home</li>
+          <li className="navMenuItem">About</li>
+          <li className="navMenuItem">Downloads</li>
+        </div>
     </div>
   )
 }
