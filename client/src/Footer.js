7505045948f968548cfd181cbc64889d2674faca
Anvesh <anvkup2002@gmail.com>
2022-08-31 18:26:06 +0530

    Footer code and footer css

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,50 +1,47 @@
 import React from 'react'
+import "./Footer.css"
 function Footer() {
   return (
-<<<<<<< HEAD
-    <>
-      <div class="footer-left">
+    <div>
+      <div className="footer-left">
         <h3>Company<span>logo</span></h3>
-        <p class="footer-links">
-          <a href="#" class="link-1">Home</a>
+        <p className="footer-links">
+          <a href="#" className="link-1">Home</a>
           <a href="#">Blog</a>
           <a href="#">Pricing</a>
           <a href="#">About</a>
           <a href="#">Faq</a>
           <a href="#">Contact</a>
         </p>
-        <p class="footer-company-name">Company Name © 2015</p>
+        <p className="footer-company-name">Company Name © 2015</p>
       </div>
-      <div class="footer-center">
+      <div classNameName="footer-center">
         <div>
-          <i class="fa fa-map-marker"></i>
+          <i classNameName="fa fa-map-marker"></i>
           <p><span>444 S. Cedros Ave</span> Solana Beach, California</p>
         </div>
         <div>
-          <i class="fa fa-phone"></i>
+          <i classNameName="fa fa-phone"></i>
           <p>+1.555.555.5555</p>
         </div>
         <div>
-          <i class="fa fa-envelope"></i>
+          <i classNameName="fa fa-envelope"></i>
           <p><a href="mailto:support@company.com">support@company.com</a></p>
         </div>
       </div>
-      <div class="footer-right">
-        <p class="footer-company-about">
+      <div classNameName="footer-right">
+        <p classNameName="footer-company-about">
           <span>About the company</span>
           Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
         </p>
-        <div class="footer-icons">
-          <a href="#"><i class="fa fa-facebook"></i></a>
-          <a href="#"><i class="fa fa-twitter"></i></a>
-          <a href="#"><i class="fa fa-linkedin"></i></a>
-          <a href="#"><i class="fa fa-github"></i></a>
+        <div classNameName="footer-icons">
+          <a href="#"><i classNameName="fa fa-facebook"></i></a>
+          <a href="#"><i classNameName="fa fa-twitter"></i></a>
+          <a href="#"><i classNameName="fa fa-linkedin"></i></a>
+          <a href="#"><i classNameName="fa fa-github"></i></a>
         </div>
       </div>
-    </>
-=======
-    <div></div>
->>>>>>> afd25b5299e12e735dac239db44523aebd5a9e51
+    </div>
   )
 }
 export default Footer
