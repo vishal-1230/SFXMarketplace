d40b737fb4e94e27842862a593bf563c48e0003f
Vishal Vishwajeet <vishalvishwajeet422@gmail.com>
2022-08-31 17:43:47 +0530

    Made Components

---
+++ client/src/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+import MainComponent from './MainComponent.js'
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+        <Navbar />
+        <MainComponent />
+    </div>
+)
+}
+
+export default Dashboard
