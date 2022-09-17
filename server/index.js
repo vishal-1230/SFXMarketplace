6d23aa635976fde28580f419e55a696f30e429fd
vishal-1230 <45381049+vishal-1230@users.noreply.github.com>
2022-09-17 16:00:36 +0530

    linked mongodb

--- server/index.js
+++ server/index.js
@@ -1,2 +1,24 @@
 import express from express
 import mongoose from "mongoose"
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+
+const app=express()
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+app.use(cors({
+    origin:'http://localhost:3000'
+}))
+dotenv.config()
+
+const port=process.env.PORT || 8080
+
+
+mongoose.connect('mongodb+srv://vishal:Cluster2004@cluster0.uy65a.mongodb.net/sfxm?retryWrites=true&w=majority')
+var db=mongoose.connection
+
+
+
+app.listen(port)
