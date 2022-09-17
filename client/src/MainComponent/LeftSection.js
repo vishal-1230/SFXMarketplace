5f12b185be6878cb4665817fece627ae228b9306
Vishal Vishwajeet <vishalvishwajeet422@gmail.com>
2022-09-17 13:23:48 +0530

    trial commit for bootstrap integration

--- client/src/MainComponent/LeftSection.js
+++ client/src/MainComponent/LeftSection.js
@@ -8,11 +8,21 @@ function LeftSection() {
         <h2>Filters</h2><br />
         <div className="genreDiv">
             <span className="filterGenreTitle">Genre</span><br />
-            <input type="text"  className="genreInput" placeholder='Search Genre' /><br />
+            {/* <input type="text"  className="genreInput" placeholder='Search Genre' /><br />
             <div className="genreDropdown">
 
-            </div><br />
-        </div>
+            </div><br /> */}
+            <div class="btn-group">
+              <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
+                  Clickable inside
+              </button>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Menu item</a></li>
+                <li><a class="dropdown-item" href="#">Menu item</a></li>
+                <li><a class="dropdown-item" href="#">Menu item</a></li>
+              </ul>
+            </div>
+        </div><br />
         <div className="keyDiv">
             <span className="filterKeyTitle">Key</span><br />
             <input type="text" placeholder='Search Key' className="keyInput" />
