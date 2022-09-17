a5e6fed5f1ce6d7ae71c96d79c7fcccd775d1fee
Vishal Vishwajeet <vishalvishwajeet422@gmail.com>
2022-09-17 13:00:08 +0530

    created left section basic filters

--- client/src/MainComponent.js
+++ client/src/MainComponent.js
@@ -6,8 +6,21 @@ import Cards from './MainComponent/Cards'
 function MainComponent() {
   return (
     <div className="main">
-      <LeftSection />
-      <Cards />
+      <div className="categories">
+        {/* <ul className="catsList"> */}
+          <li id="1" className='catItem'>Lorem</li>
+          <li id="2" className='catItem'>Lorem ips</li>
+          <li id="3" className='catItem'>Lorem, ipsum</li>
+          <li id="4" className='catItem'>Lorem </li>
+          <li id="5" className='catItem'>Ipsum</li>
+          <li id="6" className='catItem'>Ipsum</li>
+          <li id="7" className='catItem'>Ipsum</li>
+        {/* </ul> */}
+      </div>
+      <div className="content">
+        <LeftSection />
+        <Cards />
+      </div>
     </div>
   )
 }
--- client/src/MainComponent/LeftSection.js
+++ client/src/MainComponent/LeftSection.js
@@ -6,10 +6,10 @@ function LeftSection() {
   return (
     <div className="leftSection">
         <h2>Filters</h2><br />
-        <div className="categoryDiv">
-            <span className="filterCategoryTitle">Category</span><br />
-            <input type="text"  className="categoryInput" placeholder='Search Category' /><br />
-            <div className="categoriesDropdown">
+        <div className="genreDiv">
+            <span className="filterGenreTitle">Genre</span><br />
+            <input type="text"  className="genreInput" placeholder='Search Genre' /><br />
+            <div className="genreDropdown">
 
             </div><br />
         </div>
@@ -21,8 +21,13 @@ function LeftSection() {
             </div><br />
         </div>
         <div className="bpmDiv">
+          <span className="bpmTitle">BPM / Tempo</span><br />
+          <input type="text" placeholder='BPM or Tempo' className="bpmInput" id='bpmInput' /><br />
+          <div className="bpmDropdown">
 
+          </div><br />
         </div>
+        <br />
     </div>
   )
 }
