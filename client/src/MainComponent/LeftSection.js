afcc3c1ee86bd524a8770017d3ac3c6b5635be94
Vishal Vishwajeet <vishalvishwajeet422@gmail.com>
2022-09-17 12:28:32 +0530

    created gitignore for server

--- client/src/MainComponent/LeftSection.js
+++ client/src/MainComponent/LeftSection.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import './Leftsecton.css'
+import {HiOutlineFilter} from 'react-icons/hi'
 
 function LeftSection() {
   return (
     <div className="leftSection">
-        Left
+        <h2>Filters</h2><br />
+        <div className="categoryDiv">
+            <span className="filterCategoryTitle">Category</span><br />
+            <input type="text"  className="categoryInput" placeholder='Search Category' /><br />
+            <div className="categoriesDropdown">
+
+            </div><br />
+        </div>
+        <div className="keyDiv">
+            <span className="filterKeyTitle">Key</span><br />
+            <input type="text" placeholder='Search Key' className="keyInput" />
+            <div className="keysDropdown">
+            
+            </div><br />
+        </div>
+        <div className="bpmDiv">
+
+        </div>
     </div>
   )
 }
