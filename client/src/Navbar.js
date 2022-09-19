577f0cf9a2a302b1a6283afbadf7471a51c84e71
Vishal Vishwajeet <vishalvishwajeet422@gmail.com>
2022-09-19 16:31:46 +0530

    .

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -9,10 +9,10 @@ function Navbar() {
 
         <span className='navbarHeading'>SFX<span className='navbarHeading2'>Marketplace</span></span>
           <div className="navMenu">
-          <a to='/' className='a navLinks'><li className="navMenuItem">Home</li></a>
-          <a to='/about' className='a navLinks'><li className="navMenuItem">About</li></a>
-          <a to='/downloads' className='a navLinks'><li className="navMenuItem">Downloads</li></a>
-          <a to='/signuplogin' className='a navLinks'><li className="navMenuItem"><button className="signinBtn">Sign In</button></li></a>
+          <a href='/' className='a navLinks'><li className="navMenuItem">Home</li></a>
+          <a href='/about' className='a navLinks'><li className="navMenuItem">About</li></a>
+          <a href='/downloads' className='a navLinks'><li className="navMenuItem">Downloads</li></a>
+          <a href='/signuplogin' className='a navLinks'><li className="navMenuItem"><button className="signinBtn">Sign In</button></li></a>
         </div>
     </div>
   )
